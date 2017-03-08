73fc09535dfe12d91321c7dffc9f9e708a576d2c
Desktop-Programmer <wtrprogrammer@gmail.com>
2017-03-08 15:07:44 -0800

    ok

--- src/org/usfirst/frc5124/WestTorranceSadbotics2017/subsystems/Drivetrain.java
+++ src/org/usfirst/frc5124/WestTorranceSadbotics2017/subsystems/Drivetrain.java
@@ -1,15 +1,8 @@
 package org.usfirst.frc5124.WestTorranceSadbotics2017.subsystems;
 
-import java.awt.Robot;
-
 import org.usfirst.frc5124.WestTorranceSadbotics2017.RobotMap;
 import org.usfirst.frc5124.WestTorranceSadbotics2017.commands.*;
-import edu.wpi.first.wpilibj.ADXL362;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drivetrain extends Subsystem {
